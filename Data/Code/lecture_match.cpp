698b54df7b50461213b6667638994a09e5cd3155
allaeia <marie.guenon@laposte.net>
2013-12-11 11:24:55 +0100

    t

--- Data/Code/lecture_match.cpp
+++ Data/Code/lecture_match.cpp
@@ -1,6 +1,9 @@
 #include "lecture_match.h"
+#include "Image.h"
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
@@ -8,11 +11,56 @@ lecture_match::lecture_match(string file)
 {
 	cout << "test" << endl;
 
-	fstream fichier(file);
+	fstream fichier(file.c_str());
+	int nb_img;
+	vector<Image> img;
 
 	if(fichier)
 	{
-		cout << "traitement du fichier" << endl;
+		string ligne;
+        while(getline(fichier, ligne))  // tant que l'on peut mettre la ligne dans "contenu"
+        {
+            if(ligne[0]!='#')
+            {
+                if(ligne[0]==' ')//donnees d'appariement
+                {
+
+                }else{
+                    if(ligne.length()==1)
+                    {
+                        nb_img=ligne[0];
+                    }else{
+                        int prec = 1;
+                        int cpt=0;
+                        int num=ligne[0]-'0';
+                        string id; int w; int h;
+                        for(int i=1;i<ligne.length();i++)
+                        {
+                            if(ligne[i]==' '|| ligne[i]=='x' || ligne[i]=='\t' || ligne[i]=='\n')
+                            {
+                                switch(cpt)
+                                {
+                                    case 0:
+                                        id=ligne.substr(i+1,prec);
+                                        break;
+                                    case 1:
+                                        stringstream(ligne.substr(i+1,prec)) >> w;
+                                        break;
+                                    case 2:
+                                        stringstream(ligne.substr(i+1)) >> h;
+                                        break;
+                                }
+                                cpt++;
+                                prec=i;
+                            }
+                        }
+                        Image new_img(num,id, w,h);
+                        new_img.print();
+                        img.push_back(new_img);
+                    }
+                }//donnees taille image
+            }//else commentaire
+        }
 	}else{
 		cout << "impossible d'ouvrir le fichier" << endl;
 	}
--- Data/Code/main.cpp
+++ Data/Code/main.cpp
@@ -7,5 +7,4 @@ int main(const int argc,const char* argv[])
 {
 	cout << "toto" << endl;
 	lecture_match lm = lecture_match("../Mosaics/Eglise/i.matchs");
-    lm.test();
 };
