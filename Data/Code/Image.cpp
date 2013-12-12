f5e9a7beef782dea6bc515fe708fbd793407ebf1
allaeia <marie.guenon@laposte.net>
2013-12-12 14:59:00 +0100

    i

---
+++ Data/Code/Image.cpp
@@ -0,0 +1,21 @@
+#include "Image.h"
+
+#include <iostream>
+#include "Image.h"
+using namespace std;
+
+Image::Image(int num, string id, int w, int h)
+:numero(num),identity(id),width(w),height(h)
+{
+    //ctor
+}
+
+void Image::print()
+{
+    cout << numero << " " << identity << " " << width << "x" << height << endl;
+}
+
+Image::~Image()
+{
+    //dtor
+}
---
+++ Data/Code/Image.h
@@ -0,0 +1,21 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <iostream>
+
+
+class Image
+{
+    public:
+        Image(int num, std::string id, int w, int h);
+        void print();
+        virtual ~Image();
+    protected:
+    private:
+        int numero;
+        std::string identity;
+        int width;
+        int height;
+};
+
+#endif // IMAGE_H
