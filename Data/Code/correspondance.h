19aeceab7bc2a596264fa4bc2a02934a6ffcab75
jeando <favreau@polytech.unice.fr>
2013-12-13 13:05:43 +0100

    a

---
+++ Data/Code/correspondance.h
@@ -0,0 +1,20 @@
+#ifndef CORRESPONDANCE_H
+#define CORRESPONDANCE_H
+
+#include<vector>
+#include<opencv2/core/core.hpp>
+
+template<typename T>
+class correspondance
+{
+	public:
+	T x1,x2,y1,y2;
+};
+
+template<typename T,>
+cv::Mat_<T> compute_homographie(std::vector<correspondance<T>>& appariement)
+{
+
+}
+#endif
+
